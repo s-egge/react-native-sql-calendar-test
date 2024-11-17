36a31bc38adb10aecf6a618e31f9ac8b8fd92505
s-egge <ssteele836@gmail.com>
2024-11-16 16:06:02 -0800

    react-native-paper working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react"
-import { Image, StyleSheet, Button } from "react-native"
+import { Image, StyleSheet, View } from "react-native"
 import ParallaxScrollView from "@/components/ParallaxScrollView"
-import { ThemedText } from "@/components/ThemedText"
-import { ThemedView } from "@/components/ThemedView"
 import { getAllDays, deleteAllDays } from "@/db/database"
+import { Button, Text } from "react-native-paper"
 
 const flows = ["None", "Spotting", "Light", "Medium", "Heavy"]
 
@@ -20,11 +19,6 @@ export default function HomeScreen() {
     }
   }, [showData])
 
-  function handleDeletePress() {
-    deleteAllDays()
-    setShowData(false)
-    setData(null)
-  }
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -35,26 +29,24 @@ export default function HomeScreen() {
         />
       }
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Calendar/DB Testing</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.button}>
-        <Button title="View DB Data" onPress={() => setShowData(!showData)} />
-      </ThemedView>
-      <ThemedView style={styles.button}>
-        <Button
-          title="Delete DB Data"
-          onPress={() => deleteAllDays().then(() => setShowData(false))}
-        />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+      <Text variant="displaySmall">Calendar/DB Testing</Text>
+      <Button mode="elevated" onPress={() => setShowData(!showData)}>
+        View DB Data
+      </Button>
+      <Button
+        mode="elevated"
+        onPress={() => deleteAllDays().then(() => setShowData(false))}
+      >
+        Delete DB Data
+      </Button>
+      <View style={styles.stepContainer}>
         {data &&
           data.map((day: any) => (
-            <ThemedText key={day.id}>
+            <Text variant="bodyMedium" key={day.id}>
               {day.date}: {flows[day.flow_intensity]}
-            </ThemedText>
+            </Text>
           ))}
-      </ThemedView>
+      </View>
     </ParallaxScrollView>
   )
 }
